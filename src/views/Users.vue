3e7cbb3c1cbbcee6f995af0315b66208051c1fc3
JordyMontalvo <jordyjosephmontalvo@gmail.com.com>
2025-04-01 15:15:08 -0500

    change delay search

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,19 +1,24 @@
 <template>
   <Layout>
-
     <i class="load" v-if="loading"></i>
 
     <section v-if="!loading">
-
-      <div class="notification" style="margin-bottom: 0;">
+      <div class="notification" style="margin-bottom: 0">
         <div class="container">
           <strong>{{ title }}</strong>
-          <input class="input" placeholder="Buscar por nombre" v-model="search" @input="input"> <br><br>
+          <input
+            class="input"
+            placeholder="Buscar por nombre"
+            v-model="search"
+            @input="debouncedInput"
+          />
+          <br /><br />
 
-          <small>Total disponible: USD {{ Number(totalBalance).toFixed(2) }} 
-          &nbsp;&nbsp; / &nbsp;&nbsp; Total no disponible: USD {{ totalVirtualBalance }}
-        </small>
-
+          <small
+            >Total disponible: USD
+            {{ Number(totalBalance).toFixed(2) }} &nbsp;&nbsp; / &nbsp;&nbsp;
+            Total no disponible: USD {{ totalVirtualBalance }}
+          </small>
         </div>
       </div>
 
@@ -39,41 +44,71 @@
             </thead>
             <tbody>
               <tr v-for="(user, i) in sortedUsers" :key="user.id">
-                <th>{{  totalItems- (currentPage - 1) * itemsPerPage - i  }}</th>
+                <th>{{ totalItems - (currentPage - 1) * itemsPerPage - i }}</th>
                 <td>{{ user.date | date }}</td>
-                <td style="position: relative;">
-                  <span v-if="!user.edit">{{ user.name }} {{ user.lastName }}</span>
+                <td style="position: relative">
+                  <span v-if="!user.edit"
+                    >{{ user.name }} {{ user.lastName }}</span
+                  >
 
-                  <input class="input" v-model="user._name"     placeholder="Nombre"   style="max-width: 120px;" v-if="user.edit">
-                  <input class="input" v-model="user._lastName" placeholder="Apellido" style="max-width: 120px;" v-if="user.edit"> <br>
+                  <input
+                    class="input"
+                    v-model="user._name"
+                    placeholder="Nombre"
+                    style="max-width: 120px"
+                    v-if="user.edit"
+                  />
+                  <input
+                    class="input"
+                    v-model="user._lastName"
+                    placeholder="Apellido"
+                    style="max-width: 120px"
+                    v-if="user.edit"
+                  />
+                  <br />
 
                   <a v-if="!user.edit">{{ user.dni }}</a>
 
-                  <input class="input" v-model="user._dni" placeholder="Documento" style="max-width: 120px;" v-if="user.edit">
+                  <input
+                    class="input"
+                    v-model="user._dni"
+                    placeholder="Documento"
+                    style="max-width: 120px"
+                    v-if="user.edit"
+                  />
 
-                  <input class="input" v-model= "user._password" placeholder="Contraseña" style="max-width: 240px;" v-if="user.edit"><br>
+                  <input
+                    class="input"
+                    v-model="user._password"
+                    placeholder="Contraseña"
+                    style="max-width: 240px"
+                    v-if="user.edit"
+                  /><br />
 
-                  <div style="position: absolute; top: 8px; right: -8px;">
+                  <div style="position: absolute; top: 8px; right: -8px">
                     <i
                       class="fa-regular fa-pen-to-square"
-                      style="color: #ccc; cursor: pointer; margin-right: 8px;"
+                      style="color: #ccc; cursor: pointer; margin-right: 8px"
                       v-if="!user.edit"
-                      @click="edit(user)"></i>
+                      @click="edit(user)"
+                    ></i>
                     <i
                       class="fa-solid fa-check"
-                      style="color: #ccc; cursor: pointer; margin-right: 8px;"
+                      style="color: #ccc; cursor: pointer; margin-right: 8px"
                       v-if="user.edit"
-                      @click="save(user)"></i>
+                      @click="save(user)"
+                    ></i>
                     <i
                       class="fa-solid fa-xmark"
-                      style="color: #ccc; cursor: pointer; margin-right: 8px;"
+                      style="color: #ccc; cursor: pointer; margin-right: 8px"
                       v-if="user.edit"
-                      @click="cancel(user)"></i>
-                  </div> <br>
+                      @click="cancel(user)"
+                    ></i>
+                  </div>
+                  <br />
 
                   <p v-if="user.rank">rango: {{ user.rank | _rank }}</p>
-                  tel: {{ user.phone }} <br>
-
+                  tel: {{ user.phone }} <br />
                 </td>
                 <td>
                   <span v-if="user.activated">Activado</span>
@@ -84,8 +119,13 @@
                 <td>
                   <p v-if="!user.edit">{{ user.points }}</p>
 
-                  <input class="input" v-model="user._points" placeholder="Puntos" style="max-width: 120px;" v-if="user.edit">
-
+                  <input
+                    class="input"
+                    v-model="user._points"
+                    placeholder="Puntos"
+                    style="max-width: 120px"
+                    v-if="user.edit"
+                  />
                 </td>
                 <td>
                   <!-- <span v-if="user.balance != 0">
@@ -94,16 +134,24 @@
                   {{ user.balance | money }}
                 </td>
                 <td>
-                  {{ user.virtualbalance | money }} <br>
-                  <a v-if="user.virtualbalance > 0" @click="migrate(user)">migrar saldo</a>
+                  {{ user.virtualbalance | money }} <br />
+                  <a v-if="user.virtualbalance > 0" @click="migrate(user)"
+                    >migrar saldo</a
+                  >
                 </td>
                 <td>
                   <div v-if="user.parent">
-                    {{ user.parent.name }} {{ user.parent.lastName }} <br>
+                    {{ user.parent.name }} {{ user.parent.lastName }} <br />
 
                     <a v-if="!user.edit">{{ user.parent.dni }}</a>
 
-                    <input class="input" v-model="user._parent_dni" placeholder="Documento" style="max-width: 120px;" v-if="user.edit"><br>
+                    <input
+                      class="input"
+                      v-model="user._parent_dni"
+                      placeholder="Documento"
+                      style="max-width: 120px"
+                      v-if="user.edit"
+                    /><br />
 
                     {{ user.parent.phone }}
                   </div>
@@ -114,25 +162,28 @@
           </table>
         </div>
         <div class="pagination" v-if="!loading">
-          <button @click="previousPage" :disabled="currentPage === 1">Anterior</button>
+          <button @click="previousPage" :disabled="currentPage === 1">
+            Anterior
+          </button>
           <span>Página {{ currentPage }} de {{ totalPages }}</span>
-          <button @click="nextPage" :disabled="currentPage === totalPages">Siguiente</button>
+          <button @click="nextPage" :disabled="currentPage === totalPages">
+            Siguiente
+          </button>
+        </div>
       </div>
-      </div>
-
     </section>
-
   </Layout>
 </template>
 
 <script>
-import Layout from '@/views/Layout'
-import api    from '@/api'
+import Layout from "@/views/Layout";
+import api from "@/api";
+import { debounce } from "lodash";
 
 export default {
   components: { Layout },
   data() {
-    return{
+    return {
       users: [],
 
       loading: true,
@@ -140,7 +191,7 @@ export default {
       title: null,
 
       search: null,
-      check:  false,
+      check: false,
 
       show: false,
 
@@ -151,7 +202,7 @@ export default {
       searchTimeout: null,
       totalBalance: 0,
       totalVirtualBalance: 0,
-    }
+    };
   },
   computed: {
     sortedUsers() {
@@ -159,186 +210,167 @@ export default {
     },
     balance() {
       const ret = this.users.reduce((total, user) => total + user.balance, 0);
-      return ret
+      return ret;
     },
     virtualBalance() {
+      const ret = this.users.reduce((a, b) => a + Number(b.virtualbalance), 0);
 
-      const ret = this.users.reduce((a, b) => a + Number(b.virtualbalance), 0)
-
-      return ret
+      return ret;
     },
   },
   filters: {
     date(val) {
-      return new Date(val).toLocaleDateString()
+      return new Date(val).toLocaleDateString();
       // return new Date(val).toLocaleString()
     },
     money(val) {
-      if(val == null) return val
-      return `USD ${val.toFixed(2)}`
+      if (val == null) return val;
+      return `USD ${val.toFixed(2)}`;
     },
     _rank(val) {
-      if(val == 'none')              return 'Ninguno'
-      if(val == 'active')            return 'ACTIVO'
-      if(val == 'star')              return 'MASTER'
-      if(val == 'master')            return 'PLATA'
-      if(val == 'silver')            return 'PLATINO'
-      if(val == 'gold')              return 'ORO'
-      if(val == 'sapphire')          return 'ZAFIRO'
-      if(val == 'RUBI')              return 'DIAMANTE RUBI'
-      if(val == 'DIAMANTE')          return 'DIAMANTE ESTRELLA'
-      if(val == 'DOBLE DIAMANTE')    return 'DIAMANTE DOS ESTRELLAS'
-      if(val == 'TRIPLE DIAMANTE')   return 'DIAMANTE TRES ESTRELLAS'
-      if(val == 'DIAMANTE ESTRELLA') return 'DIAMANTE CBM'
+      if (val == "none") return "Ninguno";
+      if (val == "active") return "ACTIVO";
+      if (val == "star") return "MASTER";
+      if (val == "master") return "PLATA";
+      if (val == "silver") return "PLATINO";
+      if (val == "gold") return "ORO";
+      if (val == "sapphire") return "ZAFIRO";
+      if (val == "RUBI") return "DIAMANTE RUBI";
+      if (val == "DIAMANTE") return "DIAMANTE ESTRELLA";
+      if (val == "DOBLE DIAMANTE") return "DIAMANTE DOS ESTRELLAS";
+      if (val == "TRIPLE DIAMANTE") return "DIAMANTE TRES ESTRELLAS";
+      if (val == "DIAMANTE ESTRELLA") return "DIAMANTE CBM";
     },
   },
   beforeRouteUpdate(to, from, next) {
-    this.GET(to.params.filter); next()
+    this.GET(to.params.filter);
+    next();
   },
   created() {
-    const account = JSON.parse(localStorage.getItem('session'))
+    const account = JSON.parse(localStorage.getItem("session"));
 
-    this.$store.commit('SET_ACCOUNT', account)
+    this.$store.commit("SET_ACCOUNT", account);
 
-    this.GET(this.$route.params.filter)
+    this.GET(this.$route.params.filter);
+
+    this.debouncedInput = debounce(this.input, 1000);
   },
   methods: {
     async fetchUsers() {
-       await this.GET(this.$route.params.filter); // Llama a GET para obtener usuarios
-     },
+      await this.GET(this.$route.params.filter); // Llama a GET para obtener usuarios
+    },
     async GET(filter) {
-      this.loading = true
+      this.loading = true;
 
       // GET data
-      const { data } = await api.users.GET({ 
+      const { data } = await api.users.GET({
         filter,
         page: this.currentPage,
         limit: this.itemsPerPage,
         search: this.search || undefined,
         totalBalance: this.totalBalance,
         totalVirtualBalance: this.totalVirtualBalance,
-        showAvailable: this.check
-      }); 
-      console.log({ data })
+        showAvailable: this.check,
+      });
+      console.log({ data });
 
-      this.loading = false
+      this.loading = false;
       // error
-      if(data.error && data.msg == 'invalid filter') this.$router.push('activations/all')
+      if (data.error && data.msg == "invalid filter")
+        this.$router.push("activations/all");
 
       // success
       this.users = data.users
-                    .map(i => ({
-                      ...i,
-                      sending: false,
-                      visible: true,
-                      edit: false,
-                      _name: '',
-                      _lastName: '',
-                      _dni: '',
-                      _password: '',
-                      _parent_dni: '',
-                      _points: 0,
-                    }))
-                    .reverse()
+        .map((i) => ({
+          ...i,
+          sending: false,
+          visible: true,
+          edit: false,
+          _name: "",
+          _lastName: "",
+          _dni: "",
+          _password: "",
+          _parent_dni: "",
+          _points: 0,
+        }))
+        .reverse();
 
-      this.totalItems = data.total
-      this.totalPages = data.totalPages
+      this.totalItems = data.total;
+      this.totalPages = data.totalPages;
       this.totalBalance = data.totalBalance;
       this.totalVirtualBalance = data.totalVirtualBalance;
       this.showAvailable = data.showAvailable;
 
-      if(filter == 'all')        this.title = 'Todos los usuarios'
-      if(filter == 'affiliated') this.title = 'Usuarios Afiliados'
-      if(filter == 'activated')  this.title = 'Usuarios Activados'
+      if (filter == "all") this.title = "Todos los usuarios";
+      if (filter == "affiliated") this.title = "Usuarios Afiliados";
+      if (filter == "activated") this.title = "Usuarios Activados";
     },
-    created() {
-    this.fetchUsers(); // Llama a fetchUsers al crear el componente
-  },
     async changePage(page) {
-  console.log('Changing to page:', page, 'Type:', typeof page);
-  this.currentPage = page;
-  await this.GET(this.$route.params.filter);
-},
-  async nextPage() {
-    await this.changePage(this.currentPage + 1);
-  },
-  async previousPage() {
-    await this.changePage(this.currentPage - 1);
-  },
+      console.log("Changing to page:", page, "Type:", typeof page);
+      this.currentPage = page;
+      await this.GET(this.$route.params.filter);
+    },
+    async nextPage() {
+      await this.changePage(this.currentPage + 1);
+    },
+    async previousPage() {
+      await this.changePage(this.currentPage - 1);
+    },
     async input() {
-      // Clear any existing timeout
-      if (this.searchTimeout) {
-        clearTimeout(this.searchTimeout);
-      }
-
-      // Set a new timeout
-      this.searchTimeout = setTimeout(async () => {
-        this.currentPage = 1;
-        await this.GET(this.$route.params.filter);
-      }, 300); // Wait 300ms after last keystroke before searching
+      this.GET(this.$route.params.filter);
     },
     input2() {
-  const check = this.check;
-  console.log({ check });
-
-  // Filtrar usuarios en función del estado del checkbox
-  for (let user of this.users) {
-    if (!check || (check && user.balance > 0)) {
-      user.visible = true; // Mostrar usuario
-    } else {
-      user.visible = false; // Ocultar usuario
-    }
-  }
-},
+      console.log({ check });
+    },
     async migrate(user) {
-      console.log('migrate ..')
-      console.log({ user })
+      console.log("migrate ..");
+      console.log({ user });
 
-      if(!confirm("Desea migrar el saldo no disponible?")) return
+      if (!confirm("Desea migrar el saldo no disponible?")) return;
 
-      user.balance += user.virtualbalance
-      user.virtualbalance = 0
+      user.balance += user.virtualbalance;
+      user.virtualbalance = 0;
 
-      const { data } = await api.users.POST({ action: 'migrate', id: user.id }); console.log({ data })
-
+      const { data } = await api.users.POST({ action: "migrate", id: user.id });
+      console.log({ data });
     },
     edit(user) {
-      user.edit = true
-      if(!user._name)     user._name     = user.name
-      if(!user._lastName) user._lastName = user.lastName
-      if(!user._dni)      user._dni      = user.dni
-      if(!user._points)   user._points   = user.points
+      user.edit = true;
+      if (!user._name) user._name = user.name;
+      if (!user._lastName) user._lastName = user.lastName;
+      if (!user._dni) user._dni = user.dni;
+      if (!user._points) user._points = user.points;
 
-      if(!user._parent_dni) user._parent_dni = user.parent.dni
+      if (!user._parent_dni) user._parent_dni = user.parent.dni;
     },
     async save(user) {
       // post new name
       const { data } = await api.users.POST({
-        action: 'name',
+        action: "name",
         id: user.id,
         data: {
-          _name:     user._name,
+          _name: user._name,
           _lastName: user._lastName,
-          _dni:      user._dni,
+          _dni: user._dni,
           _password: user._password,
 
           _parent_dni: user._parent_dni,
-          _points:     user._points,
-        }
-      })
+          _points: user._points,
+        },
+      });
 
-      user.name     = user._name
-      user.lastName = user._lastName
-      user.dni      = user._dni
-      user.points   = user._points
+      user.name = user._name;
+      user.lastName = user._lastName;
+      user.dni = user._dni;
+      user.points = user._points;
 
-      user.parent.dni = user._parent_dni
+      user.parent.dni = user._parent_dni;
 
-      user.edit = false
+      user.edit = false;
     },
     cancel(user) {
-      user.edit = false
+      user.edit = false;
     },
-  }
+  },
 };
 </script>
